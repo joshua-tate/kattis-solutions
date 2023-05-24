197aadafd27ed13c062dd761edd4a2a9598f5f88
Joshua Tate <trend.erg_0z@icloud.com>
2023-05-23 23:25:57 -0600

    Submitted refactored solution of conundrum

--- trivial/crypto-conundrum/README.md
+++ trivial/crypto-conundrum/README.md
@@ -4,3 +4,9 @@
 - It consists of at-most $300$ upper-case characters, and its length is a multiple of $3$
 ## Output
 - Output the number of days needed to change the cipher text to a string containing only Per's name.
+### Example
+```
+Input: SECRET
+
+Output: 4
+```
--- trivial/crypto-conundrum/conundrum.jl
+++ trivial/crypto-conundrum/conundrum.jl
@@ -1,13 +1,9 @@
 cipher_text = readline()
 
-change_count = 0
+f(acc, i) = acc + (
+    i % 3 === 1 && cipher_text[i] !== 'P'
+    || i % 3 == 2 && cipher_text[i] !== 'E'
+    || i % 3 == 0 && cipher_text[i] !== 'R'
+)
 
-for i = eachindex(cipher_text)
-    global change_count += (
-        i % 3 == 1 && cipher_text[i] !== 'P'
-        || i % 3 == 2 && cipher_text[i] !== 'E'
-        || i % 3 == 0 && cipher_text[i] !== 'R'
-    ) ? 1 : 0
-end
-
-println(change_count)
+reduce(f, eachindex(cipher_text), init=0) |> println
