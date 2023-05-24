e5beca294e67bd8f38dad177d67d636280e95ca5
Joshua Tate <trend.erg_0z@icloud.com>
2023-05-24 00:01:54 -0600

    Replaced eachindex with enumerate

--- trivial/crypto-conundrum/conundrum.jl
+++ trivial/crypto-conundrum/conundrum.jl
@@ -1,9 +1,9 @@
 cipher_text = readline()
 
-f(acc, i) = acc + (
-    i % 3 === 1 && cipher_text[i] !== 'P'
-    || i % 3 == 2 && cipher_text[i] !== 'E'
-    || i % 3 == 0 && cipher_text[i] !== 'R'
+f(acc, (i, v)) = acc + (
+    i % 3 === 1 && v !== 'P'
+    || i % 3 === 2 && v !== 'E'
+    || i % 3 === 0 && v !== 'R'
 )
 
-reduce(f, eachindex(cipher_text), init=0) |> println
+reduce(f, enumerate(cipher_text), init=0) |> println
